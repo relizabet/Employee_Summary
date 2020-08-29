ba4672c64650c92483d77b44d39d3f9405188f00
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-08-29 09:16:23 -0400

    ..

--- Develop/app.js
+++ Develop/app.js
@@ -160,7 +160,7 @@ async function getEngineer() {
   }
 }
 
-function init() {
+async function init() {
   inquirer
     .prompt([
       {
@@ -171,7 +171,6 @@ function init() {
       },
     ])
     .then(function (res) {
-      console.log(res.employee);
       if (res.employee === "Manager") {
         getManager();
       } else if (res.employee === "Intern") {
@@ -187,12 +186,3 @@ init();
 // After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; the `render` function will generate and return a block of HTML including templated divs for each employee!
 
 // After you have your html, you're now ready to create an HTML file using the HTML returned from the `render` function. Now write it to a file named `team.html` in the `output` folder. You can use the variable `outputPath` above target this location. Hint: you may need to check if the `output` folder exists and create it if it does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different information; write your code to ask different questions via inquirer depending on employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer, and Intern classes should all extend from a class named Employee; see the directions for further information. Be sure to test out each class and verify it generates an object with the correct structure and methods. This structure will be crucial in order for the provided `render` function to work! ```
-
-// function askQuestions() {
-//   // use inquirer to prompt the questions and (return) the answers
-//   return inquirer.prompt(questions);
-// }
